1014c18250f2513b5cee601d7a95d375142cfe02
Leonardo Soto M. <leo.soto@gmail.com>
2023-11-17 10:45:24 -0300

    Add HTTP Basic Auth when using the production API

--- tamagotchi/app.py
+++ tamagotchi/app.py
@@ -1,6 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .utils import required_env
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if required_env("SHINKANSEN_API_HOST") == "api.shinkansen.finance":
+        return username == required_env(
+            "HTTP_AUTH_USERNAME"
+        ) and password == required_env("HTTP_AUTH_PASSWORD")
+    else:
+        return True
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{app.instance_path}/db.sqlite"
